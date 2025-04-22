2260e1665cae2fbe89e5bb8285de07e5260977f3
Andrew Lovseth <113471723+andrewlovseth-esa@users.noreply.github.com>
2025-04-21 20:46:42 -0700

    Ravenna styling updates; kit form styling

--- components/subscribe/metro/form.php
+++ components/subscribe/metro/form.php
@@ -1,81 +1,155 @@
 <div class="subscribe">
-    <form
-        hx-post="<?php echo admin_url('admin-ajax.php?action=subscribe'); ?>"
-        hx-target=".status-update"
-        hx-swap="innerHTML"
-        hx-indicator="#spinner"
-        hx-on="htmx:afterSwap: document.querySelector('.status-update')?.classList.add('fade-in');"
-        class="subscribe__form">
 
-        <label for="email-input" class="subscribe__label">Email</label>
+<script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
 
-        <input 
-            type="email" 
-            name="email" 
-            class="subscribe__input" 
-            id="email-input" 
-            placeholder="hello@gmail.com"
-            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-            title="Email address"
-            autocomplete="email"
-            data-1p-ignore 
-            required 
-        />
-        <input type="hidden" name="email_list_id" value="67f01c5c-2fd0-47dc-9338-df47135122cf">
-
-        <button type="submit" class="subscribe__button"><span>+</span></button>
-    </form>
-
-    <div class="subscribe__receipt">
-        <div class="subscribe__receipt-header">
-            <h5 class="subscribe__receipt-title">Subscription Receipt</h5>
-            <h5 class="subscribe__receipt-title">The Seattle Sun</h5>
-        </div>
-        <div class="subscribe__receipt-body">
-            <div class="subscribe__receipt-item">
-                <div class="subscribe__receipt-label">Volume:</div>
-                <div class="subscribe__receipt-value"><span class="volume">Metro</span></div>
+<form action="https://app.kit.com/forms/7931021/subscriptions" class="seva-form formkit-form" method="post" data-sv-form="7931021" data-uid="aec9279c08" data-format="inline" data-version="5" data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;fathom&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null,&quot;sparkloop&quot;:null,&quot;googletagmanager&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://kit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}" min-width="400 500 600 700 800">
+    <div data-style="clean">
+        <ul class="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
+        <div data-element="fields" data-stacked="false" class="seva-fields formkit-fields">
+            <div class="formkit-field">
+                <input class="formkit-input" name="email_address" aria-label="Email Address" placeholder="Email Address" required="" type="email" style="color: rgb(0, 0, 0); border-color: rgb(227, 227, 227); border-radius: 4px; font-weight: 400;">
             </div>
-
-            <div class="subscribe__receipt-item">
-                <div class="subscribe__receipt-label">When:</div>
-                <div class="subscribe__receipt-value"><span class="when">Mondays A.M.</span></div>
-            </div>
-
-            <div class="subscribe__receipt-item">
-                <div class="subscribe__receipt-label">Email:</div>
-                <div class="subscribe__receipt-value user-input" id="email-value">&nbsp;</div>
-            </div>
-
-            <div class="subscribe__receipt-item">
-                <div class="subscribe__receipt-label">Status:</div>
-                <div class="subscribe__receipt-value user-input">
-                    <div class="status-update">
-                        <div class="spinner htmx-indicator" id="spinner" htmx-indicator>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    </div>
+            <button data-element="submit" class="formkit-submit formkit-submit" style="color: rgb(255, 255, 255); background-color: rgb(211, 97, 53); border-radius: 4px; font-weight: 400;">
+                <div class="formkit-spinner">
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
-            </div>                
-        </div>
-        <div class="subscribe__receipt-footer">
-            <div class="subscribe__receipt-meta">
-                <p class="subscribe__receipt-meta-label">Date</p>
-                <p class="subscribe__receipt-meta-value date" id="current-date">01 Jan</p>
-            </div>
-
-            <div class="subscribe__receipt-meta">
-                <p class="subscribe__receipt-meta-label">Time</p>
-                <p class="subscribe__receipt-meta-value time" id="current-time">12:00</p>
-            </div>  
-
-            <div class="subscribe__receipt-meta">
-                <p class="subscribe__receipt-meta-label">Country</p>
-                <p class="subscribe__receipt-meta-value country" id="current-country">SEA</p>
-            </div>  
+                <span class="">Subscribe</span>
+            </button>
         </div>
     </div>
+
+    <style>
+        .formkit-form[data-uid="aec9279c08"] *{box-sizing:border-box;}
+        .formkit-form[data-uid="aec9279c08"]{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;}
+        .formkit-form[data-uid="aec9279c08"] legend{border:none;font-size:inherit;margin-bottom:10px;padding:0;position:relative;display:table;}
+        .formkit-form[data-uid="aec9279c08"] fieldset{border:0;padding:0.01em 0 0 0;margin:0;min-width:0;}
+        .formkit-form[data-uid="aec9279c08"] body:not(:-moz-handler-blocked) fieldset{display:table-cell;}
+        .formkit-form[data-uid="aec9279c08"] h1,.formkit-form[data-uid="aec9279c08"] h2,.formkit-form[data-uid="aec9279c08"] h3,.formkit-form[data-uid="aec9279c08"] h4,.formkit-form[data-uid="aec9279c08"] h5,.formkit-form[data-uid="aec9279c08"] h6{color:inherit;font-size:inherit;font-weight:inherit;}
+        .formkit-form[data-uid="aec9279c08"] h2{font-size:1.5em;margin:1em 0;}
+        .formkit-form[data-uid="aec9279c08"] h3{font-size:1.17em;margin:1em 0;}
+        .formkit-form[data-uid="aec9279c08"] p{color:inherit;font-size:inherit;font-weight:inherit;}
+        .formkit-form[data-uid="aec9279c08"] ol:not([template-default]),.formkit-form[data-uid="aec9279c08"] ul:not([template-default]),.formkit-form[data-uid="aec9279c08"] blockquote:not([template-default]){text-align:left;}
+        .formkit-form[data-uid="aec9279c08"] p:not([template-default]),.formkit-form[data-uid="aec9279c08"] hr:not([template-default]),.formkit-form[data-uid="aec9279c08"] blockquote:not([template-default]),.formkit-form[data-uid="aec9279c08"] ol:not([template-default]),.formkit-form[data-uid="aec9279c08"] ul:not([template-default]){color:inherit;font-style:initial;}
+        .formkit-form[data-uid="aec9279c08"] .ordered-list,
+        .formkit-form[data-uid="aec9279c08"] .unordered-list{list-style-position:outside !important;padding-left:1em;}
+        .formkit-form[data-uid="aec9279c08"] .list-item{padding-left:0;}
+
+        .formkit-form[data-uid="aec9279c08"][data-format="modal"]{display:none;}
+        .formkit-form[data-uid="aec9279c08"][data-format="slide in"]{display:none;}
+        .formkit-form[data-uid="aec9279c08"][data-format="sticky bar"]{display:none;}
+        .formkit-sticky-bar .formkit-form[data-uid="aec9279c08"][data-format="sticky bar"]{display:block;}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-input,
+        .formkit-form[data-uid="aec9279c08"] .formkit-select,
+        .formkit-form[data-uid="aec9279c08"] .formkit-checkboxes{width:100%;}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-button,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit{border:0;border-radius:5px;color:#ffffff;cursor:pointer;display:inline-block;text-align:center;font-size:15px;font-weight:500;cursor:pointer;margin-bottom:15px;overflow:hidden;padding:0;position:relative;vertical-align:middle;}
+        
+        .formkit-form[data-uid="aec9279c08"] .formkit-button:hover,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit:hover,
+        .formkit-form[data-uid="aec9279c08"] .formkit-button:focus,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit:focus{outline:none;}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-button:hover > span,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit:hover > span,
+        .formkit-form[data-uid="aec9279c08"] .formkit-button:focus > span,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit:focus > span{background-color:rgba(0,0,0,0.1);}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-button > span,
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit > span{display:block;-webkit-transition:all 300ms ease-in-out;transition:all 300ms ease-in-out;padding:12px 24px;}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-input{background:#ffffff;font-size:15px;padding:12px;border:1px solid #e3e3e3;-webkit-flex:1 0 auto;-ms-flex:1 0 auto;flex:1 0 auto;line-height:1.4;margin:0;-webkit-transition:border-color ease-out 300ms;transition:border-color ease-out 300ms;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-input:focus{outline:none;border-color:#1677be;-webkit-transition:border-color ease 300ms;transition:border-color ease 300ms;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-input::-webkit-input-placeholder{color:inherit;opacity:0.8;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-input::-moz-placeholder{color:inherit;opacity:0.8;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-input:-ms-input-placeholder{color:inherit;opacity:0.8;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-input::placeholder{color:inherit;opacity:0.8;}
+
+        .formkit-form[data-uid="aec9279c08"] [data-group="dropdown"]{position:relative;display:inline-block;width:100%;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="dropdown"]::before{content:"";top:calc(50% - 2.5px);right:10px;position:absolute;pointer-events:none;border-color:#4f4f4f transparent transparent transparent;border-style:solid;border-width:6px 6px 0 6px;height:0;width:0;z-index:999;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="dropdown"] select{height:auto;width:100%;cursor:pointer;color:#333333;line-height:1.4;margin-bottom:0;padding:0 6px;-webkit-appearance:none;-moz-appearance:none;appearance:none;font-size:15px;padding:12px;padding-right:25px;border:1px solid #e3e3e3;background:#ffffff;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="dropdown"] select:focus{outline:none;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"]{text-align:left;margin:0;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"]{margin-bottom:10px;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] *{cursor:pointer;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"]:last-of-type{margin-bottom:0;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] input[type="checkbox"]{display:none;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] input[type="checkbox"] + label::after{content:none;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] input[type="checkbox"]:checked + label::after{border-color:#ffffff;content:"";}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] input[type="checkbox"]:checked + label::before{background:#10bf7a;border-color:#10bf7a;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] label{position:relative;display:inline-block;padding-left:28px;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] label::before,.formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] label::after{position:absolute;content:"";display:inline-block;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] label::before{height:16px;width:16px;border:1px solid #e3e3e3;background:#ffffff;left:0px;top:3px;}
+        .formkit-form[data-uid="aec9279c08"] [data-group="checkboxes"] [data-group="checkbox"] label::after{height:4px;width:8px;border-left:2px solid #4d4d4d;border-bottom:2px solid #4d4d4d;-webkit-transform:rotate(-45deg);-ms-transform:rotate(-45deg);transform:rotate(-45deg);left:4px;top:8px;}
+        
+        .formkit-form[data-uid="aec9279c08"] .formkit-alert{background:#f9fafb;border:1px solid #e3e3e3;border-radius:5px;-webkit-flex:1 0 auto;-ms-flex:1 0 auto;flex:1 0 auto;list-style:none;margin:25px auto;padding:12px;text-align:center;width:100%;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-alert:empty{display:none;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-alert-success{background:#d3fbeb;border-color:#10bf7a;color:#0c905c;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-alert-error{background:#fde8e2;border-color:#f2643b;color:#ea4110;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-spinner{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;height:0px;width:0px;margin:0 auto;position:absolute;top:0;left:0;right:0;width:0px;overflow:hidden;text-align:center;-webkit-transition:all 300ms ease-in-out;transition:all 300ms ease-in-out;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-spinner > div{margin:auto;width:12px;height:12px;background-color:#fff;opacity:0.3;border-radius:100%;display:inline-block;-webkit-animation:formkit-bouncedelay-formkit-form-data-uid-aec9279c08- 1.4s infinite ease-in-out both;animation:formkit-bouncedelay-formkit-form-data-uid-aec9279c08- 1.4s infinite ease-in-out both;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-spinner > div:nth-child(1){-webkit-animation-delay:-0.32s;animation-delay:-0.32s;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-spinner > div:nth-child(2){-webkit-animation-delay:-0.16s;animation-delay:-0.16s;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit[data-active] .formkit-spinner{opacity:1;height:100%;width:50px;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit[data-active] .formkit-spinner ~ span{opacity:0;}
+        .formkit-form[data-uid="aec9279c08"] blockquote{padding:10px 20px;margin:0 0 20px;border-left:5px solid #e1e1e1;}
+        .formkit-form[data-uid="aec9279c08"] .seva-custom-content{padding:15px;font-size:16px;color:#fff;mix-blend-mode:difference;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-modal.guard{max-width:420px;width:100%;} .formkit-form[data-uid="aec9279c08"]{max-width:700px;}
+        .formkit-form[data-uid="aec9279c08"] [data-style="clean"]{width:100%;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-fields{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap;margin:0 auto;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-field,.formkit-form[data-uid="aec9279c08"] .formkit-submit{margin:0 0 15px 0;-webkit-flex:1 0 100%;-ms-flex:1 0 100%;flex:1 0 100%;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-powered-by-convertkit-container{margin:0;}
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit{position:static;}
+
+
+        .formkit-form[data-uid="aec9279c08"][min-width~="700"] .formkit-fields[data-stacked="false"] .formkit-field,
+        .formkit-form[data-uid="aec9279c08"][min-width~="800"] .formkit-fields[data-stacked="false"] .formkit-field,
+        .formkit-form[data-uid="aec9279c08"][min-width~="700"] .formkit-fields[data-stacked="false"] .formkit-submit,
+        .formkit-form[data-uid="aec9279c08"][min-width~="800"] .formkit-fields[data-stacked="false"] .formkit-submit{margin:0;}
+
+        .formkit-form[data-uid="aec9279c08"][min-width~="700"] .formkit-fields[data-stacked="false"] .formkit-field,
+        .formkit-form[data-uid="aec9279c08"][min-width~="800"] .formkit-fields[data-stacked="false"] .formkit-field{-webkit-flex:100 1 auto;-ms-flex:100 1 auto;flex:100 1 auto;}
+
+        .formkit-form[data-uid="aec9279c08"][min-width~="700"] .formkit-fields[data-stacked="false"] .formkit-submit,
+        .formkit-form[data-uid="aec9279c08"][min-width~="800"] .formkit-fields[data-stacked="false"] .formkit-submit{-webkit-flex:1 1 auto;-ms-flex:1 1 auto;flex:1 1 auto;} .formkit-form[data-uid="aec9279c08"] .formkit-powered-by-convertkit-container{display:none;}
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-input{
+            font-family:"Decimal A","Decimal B",sans-serif;
+            font-weight: 400 !important;
+        }        
+
+        .formkit-fields {
+            display: grid;
+            gap: 0.5rem;
+            grid-template-columns: 1fr auto;
+        }
+
+        .formkit-form[data-uid="aec9279c08"] .formkit-submit{
+            font-family:"Decimal A","Decimal B",sans-serif;
+            font-weight:600 !important;
+            text-transform: uppercase;
+            font-size: 0.75rem;
+            letter-spacing: 0.05em;
+        }
+
+        form.formkit-form[data-format="inline"] {
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 0 !important;
+            width: 100% !important;
+            max-width: 100% !important;
+        }
+        @media only screen and (min-width: 482px) {
+            form.formkit-form[data-format="inline"] {
+                margin-bottom: 0 !important;
+            }
+        }
+
+</style>
+</form>
+
+
 </div>
--- functions/email-subscription.php
+++ functions/email-subscription.php
@@ -4,119 +4,90 @@ add_action('wp_ajax_nopriv_subscribe', 'handle_email_subscription');
 add_action('wp_ajax_subscribe', 'handle_email_subscription');
 
 function handle_email_subscription() {
+    header('Content-Type: application/json');
+
     try {
-        // Input validation
+        // Log full incoming request for debugging
+        error_log('[ConvertKit] Incoming POST: ' . print_r($_POST, true));
+
+        // Validate email
+        if (empty($_POST['email'])) {
+            wp_send_json_error(['message' => 'Email is required.'], 400);
+            return;
+        }
+
         $email = sanitize_email($_POST['email']);
-        $email_list_id = sanitize_text_field($_POST['email_list_id']);
-
         if (!is_email($email)) {
-            header('HTTP/1.1 400 Bad Request');
-            $args = ['message' => 'Invalid email address.'];
-            get_template_part('components/subscribe/metro/error', null, $args);
-            exit;
+            wp_send_json_error(['message' => 'Invalid email address.'], 400);
+            return;
         }
 
-        if (empty($email_list_id)) {
-            header('HTTP/1.1 400 Bad Request');
-            $args = ['message' => 'Email list ID is required.'];
-            get_template_part('components/subscribe/metro/error', null, $args);
-            exit;
+        // Load ConvertKit personal access token
+        $access_token = get_field('kit_key', 'option');
+
+        if (empty($access_token)) {
+            wp_send_json_error(['message' => 'Server misconfiguration. Please contact support.'], 500);
+            return;
         }
 
-        // Get Supabase credentials
-        $supabase_url = get_field('supabase_url', 'option');
-        $supabase_key = get_field('supabase_key', 'option');
+        // Build request body
+        $request_body = [
+            'email_address' => $email,
+            'state' => 'active',
+            'fields' => [
+                'Last Name' => '',
+                'Birthday' => '',
+                'Source' => 'website'
+            ]
+        ];
 
-
-        // Step 1: First try to get the existing subscriber
-        $response = wp_remote_get("$supabase_url/rest/v1/email_subscribers?email=eq.$email&select=id", array(
-            'headers' => array(
-                'Authorization' => "Bearer $supabase_key",
-                'apikey' => $supabase_key,
-                'Content-Type' => 'application/json'
-            )
-        ));
+        // Send API request
+        $response = wp_remote_post('https://api.kit.com/v4/subscribers', [
+            'headers' => [
+                'Content-Type'  => 'application/json',
+                'Accept'        => 'application/json',
+                'Authorization' => 'Bearer ' . $access_token
+            ],
+            'body'    => json_encode($request_body),
+            'timeout' => 30,
+            'sslverify' => true
+        ]);
 
         if (is_wp_error($response)) {
-            header('HTTP/1.1 500 Internal Server Error');
-            $args = ['message' => 'Error checking subscriber. Please try again later.'];
-            get_template_part('components/subscribe/metro/error', null, $args);
-            exit;
+            wp_send_json_error(['message' => 'Unable to connect to the subscription service.'], 500);
+            return;
         }
 
-        $existing_subscriber = json_decode(wp_remote_retrieve_body($response), true);
+        $status_code = wp_remote_retrieve_response_code($response);
+        $body = json_decode(wp_remote_retrieve_body($response), true);
 
-        if (empty($existing_subscriber)) {
-            // If subscriber doesn't exist, create new one
-            $response = wp_remote_post("$supabase_url/rest/v1/email_subscribers", array(
-                'headers' => array(
-                    'Authorization' => "Bearer $supabase_key",
-                    'apikey' => $supabase_key,
-                    'Content-Type' => 'application/json',
-                    'Prefer' => 'return=representation'
-                ),
-                'body' => json_encode(array(
-                    'email' => $email,
-                    'status' => 'active'
-                )),
-            ));
-
-            if (is_wp_error($response)) {
-                header('HTTP/1.1 500 Internal Server Error');
-                $args = ['message' => 'Error saving subscriber. Please try again later.'];
-                get_template_part('components/subscribe/metro/error', null, $args);
-                exit;
-            }
-
-            $subscriber = json_decode(wp_remote_retrieve_body($response), true);
-            $subscriber_id = $subscriber[0]['id'] ?? null;
-
-            if (!$subscriber_id) {
-                header('HTTP/1.1 500 Internal Server Error');
-                $args = ['message' => 'Failed to create subscriber.'];
-                get_template_part('components/subscribe/metro/error', null, $args);
-                exit;
-            }
+        if ($status_code === 201) {
+            wp_send_json_success(['message' => 'Successfully subscribed!']);
         } else {
-            // Use existing subscriber's ID
-            $subscriber_id = $existing_subscriber[0]['id'] ?? null;
+            // Capture the best possible error message
+            $error_message = 'An error occurred.';
+            if (isset($body['message'])) {
+                $error_message = $body['message'];
+            } elseif (isset($body['errors'])) {
+                $error_message = json_encode($body['errors']);
+            }
+
+            // Log authentication errors specifically
+            if ($status_code === 401) {
+                error_log('[ConvertKit] Authentication failed. Please verify API token is valid.');
+                error_log('[ConvertKit] Token used: ' . substr($access_token, 0, 5) . '...');
+            }
+
+            // Include debug data in response
+            wp_send_json_error([
+                'message' => "Subscription failed.",
+                'status_code' => $status_code,
+                'error_details' => $error_message,
+                'response' => $body
+            ], $status_code);
         }
-
-        if (!$subscriber_id) {
-            header('HTTP/1.1 500 Internal Server Error');
-            $args = ['message' => 'Failed to retrieve subscriber information.'];
-            get_template_part('components/subscribe/metro/error', null, $args);
-            exit;
-        }
-
-        // Step 2: Subscribe the user to the selected email list
-        $response = wp_remote_post("$supabase_url/rest/v1/email_list_subscribers", array(
-            'headers' => array(
-                'Authorization' => "Bearer $supabase_key",
-                'apikey' => $supabase_key,
-                'Content-Type' => 'application/json',
-            ),
-            'body' => json_encode(array(
-                'email_list_id' => $email_list_id,
-                'subscriber_id' => $subscriber_id
-            )),
-        ));
-
-        if (is_wp_error($response)) {
-            header('HTTP/1.1 500 Internal Server Error');
-            $args = ['message' => 'Error subscribing to the list. Please try again later.'];
-            get_template_part('components/subscribe/metro/error', null, $args);
-            exit;
-        }
-
-        // Success response
-        get_template_part('components/subscribe/metro/success');
-        exit;
 
     } catch (Exception $e) {
-        header('HTTP/1.1 500 Internal Server Error');
-        $args = ['message' => 'Error subscribing to the list. Please try again later.'];
-        get_template_part('components/subscribe/metro/error', null, $args);
-        exit;
+        wp_send_json_error(['message' => 'Unexpected error. Please try again later.'], 500);
     }
 }
