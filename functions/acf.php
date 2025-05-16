85df7712c36d39d36336a36fa7cc40f07049a2a3
Andrew Lovseth <113471723+andrewlovseth-esa@users.noreply.github.com>
2025-05-15 20:52:16 -0700

    ACF back-end updates

--- functions/acf.php
+++ functions/acf.php
@@ -11,6 +11,13 @@ function bearsmith_relationship_order_by_date($args, $field, $post_id) {
     return $args;
 }
 add_filter('acf/fields/relationship/query', 'bearsmith_relationship_order_by_date', 10, 3);
+// Order Post Object fields
+function bearsmith_post_object_order_by_date($args, $field, $post_id) {
+    $args['orderby'] = 'date';
+    $args['order'] = 'DESC';
+    return $args;
+}
+add_filter('acf/fields/post_object/query', 'bearsmith_post_object_order_by_date', 10, 3);
 
 
 // Custom back-end styles
